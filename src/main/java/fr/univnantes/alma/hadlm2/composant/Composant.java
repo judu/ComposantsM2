6bc1826c371d9752f99fd2203d35b1a6649d206f
Julien Durillon <julien.durillon@kan-op.com>
2010-11-14 05:20:12 +0100

    M2 à améliorer pour le run -> ajouter gestion annotations dans call

--- src/main/java/fr/univnantes/alma/hadlm2/composant/Composant.java
+++ src/main/java/fr/univnantes/alma/hadlm2/composant/Composant.java
@@ -1,5 +1,8 @@
 package fr.univnantes.alma.hadlm2.composant;
 
+import fr.alma.hadl.annotations.ProvidedInterface;
+import fr.alma.hadl.annotations.RequiredInterface;
+import fr.alma.hadl.annotations.RunInterface;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -13,48 +16,92 @@ import java.util.logging.Logger;
  */
 public abstract class Composant extends Observable {
 
-    protected Configuration parent;
+   protected Configuration parent;
 
-    public void call(String service) {
-        this.setChanged();
-        this.notifyObservers(service);
-    }
+   public void call(String service) {
+      this.setChanged();
+      this.notifyObservers(service);
+   }
 
-    public void set(String port, Object value) {
-        for (Field f : this.getClass().getDeclaredFields()) {
-            if (f.getName().equals(port)
-                    && f.getType().equals(value.getClass())) {
-                try {
-                    f.set(this, value);
-                } catch (IllegalArgumentException ex) {
-                    Logger.getLogger(Composant.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IllegalAccessException ex) {
-                    Logger.getLogger(Composant.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                this.call(port);
-                break;
-            } // if
-        } // for
-    }
-
-    protected final <I extends AccessibleObject> boolean hasInterface(I iface) {
-        if (iface.getClass().equals(Field.class)) {
-            for (Field f : this.getClass().getDeclaredFields()) {
-                if (f.equals(iface)) {
-                    return true;
-                }
+   public void set(String port, Object value) {
+      for (Field f : this.getClass().getDeclaredFields()) {
+         if (f.getName().equals(port)
+                 && f.getType().equals(value.getClass())) {
+            try {
+               f.set(this, value);
+            } catch (IllegalArgumentException ex) {
+               Logger.getLogger(Composant.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+               Logger.getLogger(Composant.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } else if (iface.getClass().equals(Method.class)) {
-            for (Method m : this.getClass().getDeclaredMethods()) {
-                if (m.equals(iface)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+            this.call(port);
+            break;
+         } // if
+      } // for
+   }
 
-    public void setParent(Configuration c) {
-        parent = c;
-    }
+   protected final <I extends AccessibleObject> boolean hasInterface(I iface) {
+      if (iface.getClass().equals(Field.class)) {
+         for (Field f : this.getClass().getDeclaredFields()) {
+            if (f.equals(iface)) {
+               return true;
+            }
+         }
+      } else if (iface.getClass().equals(Method.class)) {
+         for (Method m : this.getClass().getDeclaredMethods()) {
+            if (m.equals(iface)) {
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+
+   public final AccessibleObject getInterfaceForName(String name, Boolean from) {
+      Field[] fs = this.getClass().getDeclaredFields();
+
+      for (int i = 0; i < fs.length; ++i) {
+         if (from) {
+            ProvidedInterface annot = fs[i].getAnnotation(ProvidedInterface.class);
+            if ((annot != null) && annot.value().equals(name)) {
+               return fs[i];
+            }
+         } else {
+            RequiredInterface annot = fs[i].getAnnotation(RequiredInterface.class);
+            if (annot != null && annot.value().equals(name)) {
+               return fs[i];
+            }
+         }
+      }
+
+      Method[] ms = this.getClass().getDeclaredMethods();
+      for (int i = 0; i < ms.length; ++i) {
+         if (from) {
+            ProvidedInterface annot = ms[i].getAnnotation(ProvidedInterface.class);
+            if ((annot != null) && annot.value().equals(name)) {
+               return ms[i];
+            }
+         } else {
+            RequiredInterface annot = ms[i].getAnnotation(RequiredInterface.class);
+            if (annot != null && annot.value().equals(name)) {
+               return ms[i];
+            }
+         }
+      }
+      return null;
+   }
+
+   public final Method getEntryPoint() {
+      Method[] ms = this.getClass().getDeclaredMethods();
+      for (int i = 0; i < ms.length; ++i) {
+         if (ms[i].getAnnotation(RunInterface.class) != null) {
+            return ms[i];
+         }
+      }
+      return null;
+   }
+
+   public void setParent(Configuration c) {
+      parent = c;
+   }
 }
--- src/main/java/fr/univnantes/alma/hadlm2/composant/Configuration.java
+++ src/main/java/fr/univnantes/alma/hadlm2/composant/Configuration.java
@@ -22,155 +22,159 @@ import java.util.Observer;
  */
 public abstract class Configuration extends Composant implements Observer {
 
-    private List<Composant> composants;
-    private ConnecteurPool connecteurs;
-    private List<Binding> bindings;
+   private List<Composant> composants;
 
-    public Configuration() {
-        composants = new ArrayList<Composant>();
-        connecteurs = new ConnecteurPool();
-        bindings = new ArrayList<Binding>();
-    }
+   private ConnecteurPool connecteurs;
 
-    public void call(String service) {
-        for (Binding b : bindings) {
-            if (b.getSource() instanceof Method
-                    && ((Method) b.getSource()).getName().equals(service)) {
-                b.getTarget().call(((Method) b.getTargetInterface()).getName());
-                break;
+   private List<Binding> bindings;
+
+   public Configuration() {
+      composants = new ArrayList<Composant>();
+      connecteurs = new ConnecteurPool();
+      bindings = new ArrayList<Binding>();
+   }
+
+   @Override
+   public void call(String service) {
+      for (Binding b : bindings) {
+         if (b.getSource() instanceof Method
+                 && ((Method) b.getSource()).getName().equals(service)) {
+            b.getTarget().call(((Method) b.getTargetInterface()).getName());
+            break;
+         }
+      }
+      this.setChanged();
+      this.notifyObservers(service);
+   }
+
+   @Override
+   public void set(String port, Object value) {
+      for (Binding b : bindings) {
+         if (b.getSource() instanceof Field
+                 && ((Field) b.getSource()).getName().equals(port)
+                 && ((Field) b.getSource()).getType().equals(value.getClass())) {
+            b.getTarget().set(((Field) b.getTargetInterface()).getName(), value);
+            break;
+         }
+      }
+   }
+
+   public final List<Connecteur> getConnecteurs(Composant source, Method roleFrom) {
+      return connecteurs.get(source, roleFrom);
+   }
+
+   public final void addConnecteur(Connecteur conn) throws NoSuchComponentException {
+      Composant source = conn.getSource();
+      Composant cible = conn.getCible();
+      AccessibleObject from = conn.getRoleFrom();
+      AccessibleObject to = conn.getRoleTo();
+      checkSource:
+      {
+         for (Composant comp : composants) {
+            if (comp.equals(source) && comp.hasInterface(from)) {
+               break checkSource;
+            } // if
+         } // for
+         throw new NoSuchComponentException("No source component");
+      } // checkSource
+      checkCible:
+      {
+         for (Composant comp : composants) {
+            if (comp.equals(cible) && comp.hasInterface(to)) {
+               break checkCible;
+            } // if
+         } // for
+         throw new NoSuchComponentException("No target component");
+      } // checkCible
+      this.connecteurs.add(conn);
+   } // addConnecteur(Connecteur)
+
+   public final void addComposant(Composant comp) {
+      composants.add(comp);
+      comp.addObserver(this);
+      comp.setParent(this);
+   }
+
+   public final void addBinding(Field source, Composant target, Field targetPort)
+           throws NoSuchInterfaceException, NoSuchComponentException, WrongTypeException {
+      if (!source.getType().equals(targetPort.getType())) {
+         throw new WrongTypeException("addBinding: source field type does not match target field type.");
+      }
+      this.pushBinding(source, target, targetPort);
+   }
+
+   public final void addBinding(Method source, Composant target, Method targetService)
+           throws NoSuchInterfaceException, NoSuchComponentException, WrongTypeException {
+      if (!source.getReturnType().equals(targetService.getReturnType())) {
+         throw new WrongTypeException("addBinding: source service return type does not match target service return type.");
+      }
+      if (!Arrays.equals(source.getParameterTypes(), targetService.getParameterTypes())) {
+         throw new WrongTypeException("addBinding: source service parameters types do not match target service parameters types.");
+      }
+      this.pushBinding(source, target, targetService);
+   }
+
+   private <I extends AccessibleObject> void pushBinding(I source, Composant target, I targetInterface)
+           throws NoSuchInterfaceException, NoSuchComponentException {
+      if (!this.hasInterface(source)) {
+         throw new NoSuchInterfaceException("addBinding: no source interface.");
+      }
+      checkComposant:
+      {
+         for (Composant c : composants) {
+            if (c.equals(target)) {
+               break checkComposant;
+            } // if
+         } // for
+         throw new NoSuchComponentException("addBinding: no target component.");
+      }
+      if (!target.hasInterface(targetInterface)) {
+         throw new NoSuchInterfaceException("addBinding: no targetInterface interface in target Component.");
+      }
+      bindings.add(new Binding<I>(source, target, targetInterface));
+   }
+
+   @Override
+   public final void update(Observable o, Object arg) {
+
+      if (!(o instanceof Composant)) {
+         //TODO: add logs
+         return;
+      } // if
+
+      Composant source = (Composant) o;
+
+      String name = (String) arg;
+      Field field = null;
+      Method method = null;
+      loops:
+      {
+         for (Field f : source.getClass().getDeclaredFields()) {
+            if (f.getName().equals(name)) {
+               field = f;
+               break loops;
             }
-        }
-        this.setChanged();
-        this.notifyObservers(service);
-    }
-
-    public void set(String port, Object value) {
-        for (Binding b : bindings) {
-            if (b.getSource() instanceof Field
-                    && ((Field) b.getSource()).getName().equals(port)
-                    && ((Field) b.getSource()).getType().equals(value.getClass())) {
-                b.getTarget().set(((Field)b.getTargetInterface()).getName(),value);
-                break;
+         }
+         for (Method m : source.getClass().getDeclaredMethods()) {
+            if (m.getName().equals(name)) {
+               method = m;
+               break loops;
             }
-        }
-    }
+         }
+         return;
+      }
 
-    public final List<Connecteur> getConnecteurs(Composant source, Method roleFrom) {
-        return connecteurs.get(source, roleFrom);
-    }
+      List<Connecteur> conns = null;
+      if (field != null) {
+         conns = this.connecteurs.get(source, field);
+      } else if (method != null) {
+         conns = this.connecteurs.get(source, method);
+      }
+      for (Connecteur conn : conns) {
+         conn.glue();
+      } // for
 
-    public final void addConnecteur(Connecteur conn) throws NoSuchComponentException {
-        Composant source = conn.getSource();
-        Composant cible = conn.getCible();
-        AccessibleObject from = conn.getRoleFrom();
-        AccessibleObject to = conn.getRoleTo();
-        checkSource:
-        {
-            for (Composant comp : composants) {
-                if (comp.equals(source) && comp.hasInterface(from)) {
-                    break checkSource;
-                } // if
-            } // for
-            throw new NoSuchComponentException("No source component");
-        } // checkSource
-        checkCible:
-        {
-            for (Composant comp : composants) {
-                if (comp.equals(cible) && comp.hasInterface(to)) {
-                    break checkCible;
-                } // if
-            } // for
-            throw new NoSuchComponentException("No target component");
-        } // checkCible
-        this.connecteurs.add(conn);
-    } // addConnecteur(Connecteur)
-
-    public final void addComposant(Composant comp) {
-        composants.add(comp);
-        comp.addObserver(this);
-        comp.setParent(this);
-    }
-
-    public final void addBinding(Field source, Composant target, Field targetPort)
-            throws NoSuchInterfaceException, NoSuchComponentException, WrongTypeException {
-        if (!source.getType().equals(targetPort.getType())) {
-            throw new WrongTypeException("addBinding: source field type does not match target field type.");
-        }
-        this.pushBinding(source, target, targetPort);
-    }
-
-    public final void addBinding(Method source, Composant target, Method targetService)
-            throws NoSuchInterfaceException, NoSuchComponentException, WrongTypeException {
-        if (!source.getReturnType().equals(targetService.getReturnType())) {
-            throw new WrongTypeException("addBinding: source service return type does not match target service return type.");
-        }
-        if (!Arrays.equals(source.getParameterTypes(), targetService.getParameterTypes())) {
-            throw new WrongTypeException("addBinding: source service parameters types do not match target service parameters types.");
-        }
-        this.pushBinding(source, target, targetService);
-    }
-
-    private <I extends AccessibleObject> void pushBinding(I source, Composant target, I targetInterface)
-            throws NoSuchInterfaceException, NoSuchComponentException {
-        if (!this.hasInterface(source)) {
-            throw new NoSuchInterfaceException("addBinding: no source interface.");
-        }
-        checkComposant:
-        {
-            for (Composant c : composants) {
-                if (c.equals(target)) {
-                    break checkComposant;
-                } // if
-            } // for
-            throw new NoSuchComponentException("addBinding: no target component.");
-        }
-        if (!target.hasInterface(targetInterface)) {
-            throw new NoSuchInterfaceException("addBinding: no targetInterface interface in target Component.");
-        }
-        bindings.add(new Binding<I>(source, target, targetInterface));
-    }
-
-    @Override
-    public final void update(Observable o, Object arg) {
-
-        if (!(o instanceof Composant)) {
-            //TODO: add logs
-            return;
-        } // if
-
-        Composant source = (Composant) o;
-
-        String name = (String) arg;
-        Field field = null;
-        Method method = null;
-        loops:
-        {
-            for (Field f : source.getClass().getDeclaredFields()) {
-                if (f.getName().equals(name)) {
-                    field = f;
-                    break loops;
-                }
-            }
-            for (Method m : source.getClass().getDeclaredMethods()) {
-                if (m.getName().equals(name)) {
-                    method = m;
-                    break loops;
-                }
-            }
-            return;
-        }
-
-        List<Connecteur> conns = null;
-        if (field != null) {
-            conns = this.connecteurs.get(source, field);
-        } else if (method != null) {
-            conns = this.connecteurs.get(source, method);
-        }
-        for (Connecteur conn : conns) {
-            conn.glue();
-        } // for
-
-    }
+   }
 
    public List<Composant> getComposants() {
       return Collections.unmodifiableList(composants);
